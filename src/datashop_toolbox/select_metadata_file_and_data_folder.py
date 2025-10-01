c2d3f0b24fd50544c2057f5002d570914d50cf94
jeff-jackson-dfo <jeffrey.jackson@dfo-mpo.gc.ca>
2025-10-01 18:43:48 -0300

    Added two combo boxes

--- src/datashop_toolbox/select_metadata_file_and_data_folder.py
+++ src/datashop_toolbox/select_metadata_file_and_data_folder.py
@@ -1,6 +1,6 @@
 import sys
 from PyQt6.QtWidgets import (
-    QApplication, QMainWindow, QHBoxLayout, QVBoxLayout, QPushButton, QFileDialog, QLabel, QLineEdit, QWidget, QDialogButtonBox
+    QApplication, QMainWindow, QHBoxLayout, QVBoxLayout, QPushButton, QComboBox, QFileDialog, QLabel, QLineEdit, QWidget, QDialogButtonBox
 )
 from PyQt6.QtCore import Qt
 
@@ -15,7 +15,7 @@ class MainWindow(QMainWindow):
         self.result = None  # will hold "accept" or "reject"
 
         self.setWindowTitle("Choose Moored Thermograph Files to Process")
-        self.resize(525, 250)
+        self.resize(525, 350)
 
         self.line_edit_title = QLabel("Please enter the data processor's name in the text box below:")
         self.line_edit = QLineEdit()
@@ -26,6 +26,16 @@ class MainWindow(QMainWindow):
         self.line_edit_title.setFixedHeight(25)
         self.line_edit.setFont(font)
         self.line_edit.editingFinished.connect(self.editing_finished)
+
+        self.institution_combo = QComboBox()
+        self.institution_combo.addItems(["BIO", "FSRS"])        
+        # self.institution_combo.currentIndexChanged.connect( self.institution_index_changed ) # Sends the current index (position) of the selected item.
+        self.institution_combo.currentTextChanged.connect( self.institution_text_changed )  # Sends the current text (string) of the selected item.
+
+        self.instrument_combo = QComboBox()
+        self.instrument_combo.addItems(["Minilog", "Hobo"])
+        # self.instrument_combo.currentIndexChanged.connect( self.instrument_index_changed ) # Sends the current index (position) of the selected item.
+        self.instrument_combo.currentTextChanged.connect( self.instrument_text_changed )  # Sends the current text (string) of the selected item.
 
         self.file_button = QPushButton("Select the Metadata file\n(e.g. LFA .txt file, \nor Excel file)")
         self.file_button.setFixedSize(225, 100)
@@ -69,20 +79,26 @@ class MainWindow(QMainWindow):
         v_layout.addWidget(self.line_edit_title)
         v_layout.addWidget(self.line_edit)
 
-        # Horizontal layout for buttons to open file and folder dialogs
+        # Horizontal layout for combo boxes
         h_layout1 = QHBoxLayout()
-        h_layout1.addWidget(self.file_button)
-        h_layout1.addWidget(self.folder_button)
+        h_layout1.addWidget(self.institution_combo)
+        h_layout1.addWidget(self.instrument_combo)
+
+        # Horizontal layout for buttons to open file and folder dialogs
+        h_layout2 = QHBoxLayout()
+        h_layout2.addWidget(self.file_button)
+        h_layout2.addWidget(self.folder_button)
 
         # Horizontal layout for buttons used to close the window
-        h_layout2 = QHBoxLayout()
-        h_layout2.addStretch(1)
-        h_layout2.addWidget(self.buttonBox)
-        h_layout2.addStretch(1)
+        h_layout3 = QHBoxLayout()
+        h_layout3.addStretch(1)
+        h_layout3.addWidget(self.buttonBox)
+        h_layout3.addStretch(1)
 
         # Add horizontal layouts into vertical layout
         v_layout.addLayout(h_layout1)
         v_layout.addLayout(h_layout2)
+        v_layout.addLayout(h_layout3)
 
         container = QWidget()
         container.setLayout(v_layout)
@@ -91,6 +107,18 @@ class MainWindow(QMainWindow):
     def editing_finished(self):
             self.line_edit_text = self.line_edit.text()
             print(f"\n(1 of 3) Data processor: {self.line_edit_text}\n")
+
+    # def institution_index_changed(self, i): # i is an int
+    #     print(i)
+
+    def institution_text_changed(self, s): # s is a str
+        print(s)
+
+    # def instrument_index_changed(self, i): # i is an int
+    #     print(i)
+
+    def instrument_text_changed(self, s): # s is a str
+        print(s)
 
     def choose_metadata_file(self):
         file_path, _ = QFileDialog.getOpenFileName(self, "Select the Metadata file")
